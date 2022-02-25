8c6583054824c4e40d9fcaea6206a77f378892e5
Vitor <vbsg97@gmail.com>
2022-02-25 17:52:54 -0300

    Adicionando VO das classes do Cart

---
+++ GeekShopping.CartAPI/Data/ValueObjects/CartHeaderVO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GeekShopping.CartAPI.Data.ValueObjects
+{
+   
+    public class CartHeaderVO
+    {
+        public long Id { get; set; }    
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }  
+    }
+}
--- GeekShopping.CartAPI/Program.cs
+++ GeekShopping.CartAPI/Program.cs
@@ -1,11 +1,88 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using GeekShopping.CartAPI.Model.Context;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using GeekShopping.CartAPI.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.
+UseMySql(connection,
+            new MySqlServerVersion(
+                     new Version(8, 0, 25))));
+
+// Aqui faz o mapper das classes pro VO
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+//builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping"); // Pega do GeekShopping IdentityServer / Configuration / IdentityConfiguration.cs / ApiScopes
+    });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    }); ;
+});
+
 
 var app = builder.Build();
 
@@ -18,6 +95,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
